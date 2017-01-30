7934be2f795c8415469ef9b2f0c4dcddb5630ca2
Andrew Towers <andrewt@smallworldsocial.com>
2017-01-30 15:07:23 +1100

    Initial commit

---
+++ gen-lang.js
@@ -0,0 +1,366 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs'), basename = require('path').basename;
+const assert = require('assert');
+const log = console.log;
+const inspect = require('util').inspect;
+
+function die(msg) {
+  console.error(msg);
+  process.exit(1);
+}
+
+// ––~,–`~–{@   Grammar Parser   @}–~,–`~––
+
+var squote = "'([^\\\\']*(?:\\\\.[^\\\\']*)*)('?)",
+    dquote = '"([^\\\\"]*(?:\\\\.[^\\\\"]*)*)("?)',
+    word = "([@\\w][\\w\\d_-]*)",
+    symbols = "(\\.\\.\\.|::=|::-|.)",
+    tokenize = new RegExp("(\\r\\n?|\\n)|(\\s+)|"+squote+"|"+dquote+"|"+word+"|"+symbols, "g");
+
+const unquote_map = { "b":"\b", "f":"\f", "n":"\n", "r":"\r", "t":"\t", "v":"\v", "0":"\0", "'":"'", "\"":"\"", "\\":"\\" };
+function unquote(str) {
+  return str.replace(/\\(.)/g, (ch) => unquote_map[ch] || ch); // (x[0-9A-Fa-f][0-9A-Fa-f]|.)
+}
+
+function parse(filepath) {
+  var src = fs.readFileSync(filepath, 'utf8');
+  var ofs = 0, line = 1;
+  var re = tokenize; re.lastIndex = 0;
+  var token, value, capture, previous;
+
+  function trim(s) {
+    return s.replace(/^\s+/,'').replace(/\s+$/,'');
+  }
+
+  function error(msg) {
+    var report = basename(filepath)+":"+line+": "+msg;
+    if (previous) report += " after '"+trim(previous)+"'";
+    if (capture) report += ", found '"+trim(capture)+"'";
+    die(report);
+  }
+
+  function advance() {
+    previous = capture;
+    for (;;) {
+      var m = re.exec(src);
+      if (!m) { token = "EOF"; break; }
+      if (m[1]) { line += 1; token = "EOL"; capture = "beginning of line"; break; }
+      if (m[2]) { continue; } // whitespace.
+      capture = m[0]; // token source text for error reporting.
+      if (m[3] || m[5]) { // text literals.
+        if ((m[3] && !m[4]) || (m[5] && !m[6])) {
+          error("unterminated text literal");
+        }
+        value = unquote(m[3] || m[5]);
+        token = "text";
+        break;
+      }
+      if (m[7]) { value = m[7]; token = "word"; break; }
+      token = m[8] || ""; break;
+    }
+  }
+
+  function match(t) {
+    if (token == t) { advance(); return true; }
+    return false;
+  }
+
+  function shift() {
+    var res = value; advance(); return res;
+  }
+
+  function expect(t, what) {
+    if (token == t) { var res = value; advance(); return res; }
+    error("expecting "+(what || "'"+t+"'"));
+  }
+
+  function word(what) {
+    return expect("word", what);
+  }
+
+  // group ::= '(' alts [ '...' text:delim ] ')'
+  // maybe ::= '[' alts [ '...' text:delim ] ']'
+
+  function group(end, min) {
+    var where = line;
+    advance();
+    var seq = alts();
+    var delim = match("...") ? expect("text","a delimiter (text literal)") : null;
+    expect(end, "'"+end+"' to end group");
+    return { is:'group', alts:seq, name:null, min:min, max:1, line:where };
+  }
+
+  // ref ::= [ '%' ]:inline @word:name
+  // literal ::= @text
+  // atom ::- ref | literal | group | maybe
+
+  function atom(required) {
+    var where = line;
+    if (token == 'word' || token == '%') {
+      var inline = match("%");
+      var repr = capture; // token source text.
+      var name = word("a term-name (rule reference)");
+      return { is:'ref', to:name, name:name, inline:inline, min:1, max:1, line:where };
+    }
+    if (token == 'text') {
+      return { is:'literal', text:shift(), name:null, min:1, max:1, line:where };
+    }
+    if (token == '(') return group(')', 1);
+    if (token == '[') return group(']', 0);
+    if (required) error("expecting a term (rule-name, literal token or group expression)");
+    return null; // no match: simulate first-token-set.
+  }
+
+  // term ::= atom [ ':' name ] [ '?' | '+' | '*' ]:arity
+
+  function term(required) {
+    var t = atom(required);
+    if (!t) return null; // no match: simulate first-token-set.
+    if (match(":")) {
+      t.name = word("a name (term renaming)");
+    }
+    if (match("?")) { t.min = 0; t.max = 1; }
+    else if (match("+")) { t.min = 1; t.max = Infinity; }
+    else if (match("*")) { t.min = 0; t.max = Infinity; }
+    return t;
+  }
+
+  // sequence ::= ( term [ ':' @word:name ] [ '?' | '+' | '*' ]:arity )+
+
+  function sequence() {
+    var seq = [ term(true) ];
+    for (;;) {
+      var t = term(false);
+      if (!t) break; // no match: simulate first-token-set.
+      seq.push(t);
+    }
+    return seq;
+  }
+
+  // alts ::= sequence ( '|' sequence )*
+
+  function alts() {
+    var seq = [ sequence() ];
+    while (match("|")) {
+      seq.push(sequence());
+    }
+    return seq;
+  }
+
+  // rule ::= @EOL* word:name ( '::=' | '::-' ) alts ( @EOF | @EOL )
+
+  function rule() {
+    while (match("EOL")) {} // skip blank lines.
+    var where = line;
+    var name = word("a name at the beginning of a rule");
+    var inline = !!match("::-");
+    if (!inline) expect("::=", "::= or ::- after rule name");
+    var seq = alts();
+    if (token !== "EOF") expect("EOL", "end of line to end rule '"+name+"'");
+    return { is:'rule', name:name, alts:seq, inline:inline, line:where };
+  }
+
+  function rules() {
+    var seq = [ rule() ];
+    while (token != "EOF") {
+      seq.push(rule());
+    }
+    return seq;
+  }
+
+  // @ ::= @EOL* ( '+' ( 'ws' | 'eol' ):option @EOL* )* rule+
+
+  function start() {
+    while (match("EOL")) {} // skip blank lines.
+    while (match("+")) {
+      var option = word("an option name");
+      log("Option: +"+option);
+      while (match("EOL")) {} // skip blank lines.
+    }
+    return rules();
+  }
+
+  advance();
+  return start();
+}
+
+
+// ––~,–`~–{@   Generator   @}–~,–`~––
+
+function compile(filepath) {
+  var ruleList = parse(filepath);
+  ruleList.map(r => log(inspect(r,{depth:10})));
+
+  function error(rule, msg) {
+    die(basename(filepath)+":"+rule.line+": "+msg+" in rule '"+rule.name+"'");
+  }
+
+  // index the rules on their unique names.
+  var rules = {};
+  for (var r of ruleList) {
+    if (rules[r.name]) error(r, "duplicate rule name");
+    rules[r.name] = r;
+  }
+
+  function is_terminal(node) {
+    // references to @words are terminals.
+    return node.is === 'literal' || (node.is === 'ref' && node.to.charAt(0) === '@');
+  }
+
+  function name_for_terminal(node) {
+    assert(is_terminal(node));
+    return node.is === 'literal' ? node.text : node.to; // literal or ref to @name.
+  }
+
+  function resolve_ref(rule, ref) {
+    var to = rules[ref];
+    if (!to) error(rule, "non-terminal '"+ref+"' not found");
+    return to;
+  }
+
+  function add_terms(rule, first, seen, set) {
+    // add a set of new first-terminals to the first-set being created (for a rule).
+    // NB. all first-terminals in the resulting set must be unique, otherwise the generated
+    // parser would need to be able to back-track after partially matching a rule.
+    for (var term of set) {
+      assert(term.in_rule); // has come from first_for_seq.
+      var nom = name_for_terminal(term);
+      if (seen[nom]) {
+        var dup = seen[nom];
+        assert(dup.in_rule); // has come from first_for_seq.
+        error(rule, "ambiguous grammar: the same token '"+nom+"' appears on multiple branches:\n"+
+          "  in rule '"+dup.in_rule+"' and in rule '"+term.in_rule+"'");
+      }
+      seen[nom] = term;
+      first.push(term);
+    }
+  }
+
+  function first_for_seq(rule, seq) {
+    // attach the enclosing rule's name to every node in the sequence.
+    for (var item of seq) { item.in_rule = rule.name; }
+    // determine the set of first-terminals for a sequence.
+    assert(seq.length > 0); // sequences cannot be empty (could allow it)
+    var first = [];
+    var seen = {};
+    var epsilon = true; // until proven otherwise.
+    for (var term of seq) {
+      if (is_terminal(term)) {
+        // if the next item is a terminal, it ends our first-set UNLESS it is optional.
+        var set = [ term ];
+        add_terms(rule, first, seen, set);
+        if (term.min > 0) {
+          epsilon = false; // terminal is required; is a non-epsilon sequence.
+          break;
+        } 
+      } else if (term.is == 'ref') {
+        // if the next item is a non-terminal, its first-set is part of our first-set,
+        // and this ends our first-set UNLESS the rule-match is optional
+        // or any of the alternatives in the rule are entirely optional.
+        var to = resolve_ref(rule, term.to);
+        var set = first_for_rule(to);
+        add_terms(rule, first, seen, set);
+        assert(set.has_epsilon != null); // must be set in first_for_rule.
+        if (term.min > 0 && !set.has_epsilon) {
+          epsilon = false; // must match, and all alts have a required terminal.
+          break;
+        }
+      } else if (term.is == 'group') {
+        // if the next item is a group, its first-set part of our first-set,
+        // and this ends our first-set UNLESS the group is optional
+        // or any of the alternatives in the group are entirely optional.
+        var set = first_for_alts(rule, term.alts);
+        add_terms(rule, first, seen, set);
+        assert(set.has_epsilon != null); // must be set in first_for_alts.
+        if (term.min > 0 && !set.has_epsilon) {
+          epsilon = false; // must match, and all alts have a required terminal.
+          break;
+        }
+      } else {
+        error(rule, "bad term in sequence");
+      }
+    }
+    first.has_epsilon = epsilon;
+    return first;
+  }
+
+  function first_for_alts(rule, alts) {
+    // merge the first-sets of each alternative sequence.
+    // NB. the sets MUST be disjoint, otherwise we can't decide which alternative to parse.
+    assert(alts.length > 0); // cannot have zero alternatives.
+    var first = [];
+    var seen = {};
+    var epsilon = false; // until we find an epsilon alt.
+    for (var seq of alts) {
+      var set = first_for_seq(rule, seq);
+      add_terms(rule, first, seen, set);
+      if (set.has_epsilon) epsilon = true; // this alt can match nothing.
+    }
+    first.has_epsilon = epsilon;
+    return first;
+  }
+
+  function first_for_rule(rule) {
+    // merge the disjoint first-sets of each alternative sequence.
+    assert(rule.is === 'rule');
+    // only need to resolve the first-set once per rule.
+    if (rule.firstSet) return rule.firstSet;
+    // detect left-recursive reference cycles.
+    if (rule.resolving) error(rule, "left-recursive cycle found");
+    rule.resolving = true;
+    // a rule behaves like a group: the first-set is the union of the
+    // first-sets of the alternatives in the rule.
+    rule.firstSet = first_for_alts(rule, rule.alts);
+    // finished resolving this rule.
+    rule.resolving = false;
+    return rule.firstSet;
+  }
+
+  // find the starting rule.
+  var start = rules["@"];
+  if (!start) die(basename(filepath)+": no starting rule named '@' was found");
+
+  // build the first-sets for each rule.
+  first_for_rule(start);
+  for (var rule of ruleList) {
+    first_for_rule(rule);
+  }
+
+  // display the first-sets for all rules.
+  log("---------");
+  for (var rule of ruleList) {
+    log(rule.name+":", rule.firstSet.map(name_for_terminal));
+  }
+
+  // next things to do:
+
+  // insert wrapper objects into the first-set that represent the terminal found,
+  // with a chain of rule-activations traversed to get there; use the chain
+  // to report the full paths from the point an ambiguity is discovered.
+
+  // work out how to build an AST for infix expression operators.
+  // is left-recursion fine? (ignore cyclical states except at the leaves)
+
+  // how to generate sum-states for common terminal prefixes:
+  // args-list ::- ( name '=' expr | expr ... ',' )*
+  // expr ::= ... -> primary -> name
+
+  // what are the second-sets? is it the set of exit terminals from the states
+  // created by the first-sets? actually I think rules don't have first-sets
+  // at all, but rather we must generate states starting from the '@' rule,
+  // because the same rule can be visited in different current-states (BUT
+  // isn't the entry state of a rule always the same? (the same set of exits
+  // for the set of first-terminals?)
+}
+
+
+// ––~,–`~–{@   Main   @}–~,–`~––
+
+if (!process.argv[2]) {
+  console.error("usage: gen-lang <filename>");
+  process.exit(1);
+}
+
+process.argv.slice(2).map(compile);
---
+++ how-it-works.md
@@ -0,0 +1,121 @@
+# How Parse-Gen Works
+
+The following is a machine-readable EEBNF sufficient to parse and build an AST.
+
+Each "::=" rule becomes an AST node with a node structure named for the rule.  
+Rules declared with "::-" are semantically inlined instead (substituted at the use sites).
+
+Each non-terminal (rule reference) in a rule becomes a field of that node's structure.  
+An inlined rule ("::-") instead inserts the fields of the inlined rule at the use site.  
+The field name is the non-terminal name, unless renamed with a ":name" suffix.    
+A terminal (quoted text) can also be named with a ":name" suffix, and will generate a field.  
+Field names within a node must be unique; duplicate names generate an error.  
+A repeated field (subject to "*" or "+") generates an array field.  
+A repeated group of (more than one) fields generates an array of structures; the group must be named.   
+A non-repeated, named group of (more than one) fields also generates a structure.   
+
+The "..." operator in a group matches the terminal that follows before each repetition, i.e. a delimiter.  
+The "|" (alternatives) operator binds more tightly than the "..." operator.  
+The "%" operator forces the following non-terminal to be inlined, even if declared with "::=".  
+The rule named "@" is the starting rule for the grammar, and must be present.
+
+```
+  @        ::= rule+
+  rule     ::= br ( name | '@':name ) ( '::=' | '::-':inline ) alts eol
+  alts     ::- ( sequence ... '|' )+
+  sequence ::= clause+
+  clause   ::= atom [ ':' name ] [ '?':opt | '+':plus | '*':star ]
+  atom     ::- ref | term | group | maybe
+  ref      ::= [ '%':inline ] name
+  term     ::= text | '@' name
+  group    ::= '(' alts [ '...' delim ] ')'
+  maybe    ::= '[' alts [ '...' delim ] ']'
+  delim    ::- text
+  text     ::- @squote | @dquote
+  name     ::- @word
+  br       ::- @EOL*
+  eol      ::- @EOF | @EOL
+```
+
+Literals (quoted-text) and terminals beginning with "@" are treated as tokens for
+the purposes of parsing: they are the lexical tokens of the grammar.
+
+NB. not sure about "inlined rules" described above. If an inlined rule contains a single field
+in each alternative, I actually want to unwrap that field and rename it at the use-site,
+as in the "alts" rule above.
+
+NB. note how the 'rule' rule contains two alternatives with the same name. This should be legal,
+even though duplicate field names are illegal. In fact this pattern might be common.
+
+NB. if a group is named, and contains alternatives with only text-tokens, what gets named?
+
+NB. consider this rule, which contains a repeat inside a repeat. Does this generate an array of arrays?
+
+  alts     ::- ( clause+ ... '|' )+
+
+NB. the implementation of the rule below doesn't follow the rule itself:
+Instead, it greedily consumes EOL before deciding whether to match the group.
+
+  @ ::= ( @EOL* '+' ( 'ws' | 'eol' ) )* rule+
+
+
+First, it builds an AST for the rules in the grammar.
+The nodes in the AST are:
+
+```
+  rule {
+    name: of this rule.
+    alts: alternative sequences (list of lists of group/term/ref)
+    inline: bool, true for inlined rules, i.e. "::-"
+    line: source line number.
+  }
+  group {
+    alts: alternative sequences (list of lists of group/term/ref)
+    as: null, or the name given with the ":name" syntax.
+    min: zero for "[]", or "?" or "*" suffix, one otherwise.
+    max: Infinity for "*" or "+" suffix, one otherwise.
+    line: source line number.
+  }
+  term {
+    text: null, or the literal quoted text in the grammar.
+    name: null, or the name after '@' in the grammar.
+    repr: original source representation of the term.
+    as: null, or the name given with the ":name" syntax.
+    min: zero for "?" or "*" suffix, one otherwise.
+    max: Infinity for "*" or "+" suffix, one otherwise.
+    line: source line number.
+  }
+  ref {
+    to: name of the rule referred to.
+    as: null, or the name given with the ":name" syntax.
+    inline: bool, true for an inlined reference, i.e. "%name"
+    min: zero for "?" or "*" suffix, one otherwise.
+    max: Infinity for "*" or "+" suffix, one otherwise.
+    line: source line number.
+  }
+```
+
+### Left Recursion
+
+Left-recursive non-terminals cause a cycle in depth-first traversal from the "@" rule.
+
+Can eliminate by moving the left-recursive patterns to the end of the recursively invoked rule
+as a set of repeating (star) alternatives, and with the left-most term in each pattern elided,
+i.e. the term that caused the left-recursion.
+
+In other words, to match a left-recursive rule, we must be able to match the rule after deleting
+the left-recursive patterns; after matching it, we can repeatedly match those deleted patterns.
+For AST generation, each repeated match must "lift" the current AST node (on its left) and make
+it a field of the new AST node, and replace the lifted node with the new node.
+
+However, there are two complications:
+
+1. Other parts of the grammar might be using those same rules, with or without their own
+   recursive cycles, so we cannot just modify the rules in-place; we must copy them.
+   This can result in unused rules, so at the end of the process we must prune those.
+
+2. In the general case, left-recursion occurs as a tree of rules and groups with arbitrary depth,
+   and within that tree are left-recursive leaves and non-left-recursive leaves. It is also
+   possible that there are other left-recursive cycles within the left-recursive cycle, or an
+   arbitrary number of co-recursive cycles.
+
